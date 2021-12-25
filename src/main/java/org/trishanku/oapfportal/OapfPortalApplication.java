3050ac26957db3b8baa93357c094d29cc1e637d3
mamillapalli <ravikanthm@gmail.com>
2021-12-25 13:00:12 +0400

    Initial Commit

---
+++ src/main/java/org/trishanku/oapfportal/OapfPortalApplication.java
@@ -0,0 +1,13 @@
+package org.trishanku.oapfportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OapfPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OapfPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/trishanku/oapfportal/OapfPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.trishanku.oapfportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OapfPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
